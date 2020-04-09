90440098e17fd5e9cadd78f4a8267d0ad692f960
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2020-04-09 02:43:12 +0100

    Interface: login

--- interface.py
+++ interface.py
@@ -1,10 +1,18 @@
 import npyscreen
+import sys, socket, threading
+import chat
 
-class formsManager(npyscreen.NPSAppManaged):
-	""" we will use to manage our forms"""
+
+HOST = "Fa2y"
+PORT = chat.PORT
+CLIENT = None
+
+class App(npyscreen.NPSAppManaged):
+		
 	def onStart(self):
-		self.addForm('MAIN', mainForm,name ='Login :')
-		self.addForm('mainForm',mainForm,name = 'pchaty')
+		""" we will use to manage our forms"""
+		self.addForm('Message', mainForm,name ='Pchaty :')
+		self.addForm('MAIN',loginForm,name = 'Login :')
 		pass
 	def while_waiting(self):
 		#here we will put stuff that excute when user input state
@@ -25,19 +33,51 @@ class mainForm(npyscreen.Form):
 class msgLog(npyscreen.MultiLineEdit):
 	def display_value(self,vl):
 		return "Username : %s" %vl
-class loginForm(npyscreen.ActionForm):
+
+
+class loginForm(npyscreen.ActionPopup):
 	""" the form of login into chat app"""
-	def afterEditing(self):
-		#the next form after done with loging form
-		self.parentApp.setNextForm(mainForm)
+	# def afterEditing(self):
+	# 	#the next form after done with loging form
+	# 	self.parentApp.setNextForm("Message")
+
 	def create(self):
-		self.OK_BUTTON_TEXT = 'Connect'
-		self.server = self.add(npyscreen.TitleText,name = 'Server : ')
-		self.port = self.add(npyscreen.TitleText,name = 'Port : ')
-		self.nike = self.add(npyscreen.TitleText,name = 'NickName : ')
-		self.error = self.add(npyscreen.MultiLineEdit,value = "Message :",max_height=5, rely=6,editable=False)
+		self.show_atx = 33
+		self.show_aty = 8
+
+		self.msg = self.add(npyscreen.TitleFixedText, name = "Choose you settings:")
+		self.server1 = self.add(npyscreen.TitleText,name = 'Server : ', value = 'Fa2y')
+		self.port = self.add(npyscreen.TitleText,name = 'Port : ', value = '4040')
+		self.nick = self.add(npyscreen.TitleText,name = 'NickName : ')
 	def on_ok(self):
-		#connect fucntion
-		self.parentApp.setNextForm(mainForm)
+		#Connect
+		npyscreen.notify_wait("Connecting...")
+		global HOST, PORT,CLIENT
+		HOST = self.server1.value
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			sock.connect((HOST, PORT))
+		except:
+			npyscreen.notify_wait("Server is down or the setting are wrong, try again...")
+			return 1
+		client = chat.Client(sock)
+		client.setname(self.nick.value)
+		try:
+			chat.send_msg(sock,client.getname())
+			resp = chat.recv_msg(sock)
+			if 'Error:duplicate' in resp:
+				raise chat.ClientExsit()
+			CLIENT = client
+			self.parentApp.setNextForm('Message')
+		except (BrokenPipeError, ConnectionError):
+			sys.stdout.flush()
+			npyscreen.notify_wait("Connection Error, Try again!")
+		except chat.ClientExsit:
+			npyscreen.notify_wait("User with this name already exist")
+			self.parentApp.editw = 0
+
+	def on_cancel(self):
+		self.parentApp.setNextForm(None)
+
 if __name__ == '__main__':
-	TestApp = formsManager().run()
+	TestApp = App().run()
